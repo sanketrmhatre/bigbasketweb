b76df2dbd35a9f92e4b13ce895efb6c15f4dfbd0
sanketrmhatre <154064403+sanketrmhatre@users.noreply.github.com>
2024-01-14 14:57:22 +0530

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,572 @@
+let navbar = () =>{
+    return `<div id="navbartop">
+    <img class="navbarImage" id = "bigLogo" src="https://play-lh.googleusercontent.com/EuiZnkT8aEKjXDLX74DTp1VRIwWaeRa8Dvo-LOGAxy1FPQ8GzABTIRenksiM-A7Oz48g" alt="">
+    <i class="fa-solid fa-magnifying-glass" id="searchicon" ></i><input class="searchBox" type="text" placeholder="Search for Products...">
+        <p id="delivery">Delivery in 1 Day
+            5666441,Banglore
+      </p>
+      <button><a href="email.html" id="btna">Login/Sign Up</a></button>
+      <a href="cart.html"><i id="bagicon" class="fa-solid fa-bag-shopping"></i></a>
+      <p id = "bagcount"></p>
+</div>
+<div id="navbarbottom">
+    <select name="" id="select">
+        <option value="">Shop by Category</option>
+        <option value="">Fruits & Vegetables</option> 
+        <option value="">Foodgrains,Oil & Masala</option>
+        <option value="">Bakery, Cakes & Dairy</option>
+        <option value="">Beverages</option>
+        <option value="">Snacks & Brandes Foods</option>
+        <option value="">Beauty & Hygiene</option>
+        <option value="">Cleaning & Household</option>
+        <option value="">Kitchen, Garden & Pets</option>
+        <option value="">Eggs,Meat & Fish</option>
+        <option value="">Gourmet & World Food</option>
+        <option value="">Baby Care</option> 
+    </select>
+    <a href=""><p>Exotic Fruits & Vegg...</p></a>
+    <a href=""><p>Tea</p></a>
+    <a href=""><p>Ghee</p></a>
+    <a href=""><p>Nandhini</p></a>
+    <a href=""><p>Fresh Vegetables</p></a>
+    <img src="http://www.bigbasket.com/media/uploads/banner_images/hp_cmc_m_bbstar_28_220921_all.png" alt="">
+    <img src="http://www.bigbasket.com/media/uploads/banner_images/hp_cmc_m_offer_28_220921_all.png" alt="">
+</div>`
+}
+
+
+let navbar2 = () => {
+    return `<div id="navbartop">
+    <img class="navbarImage" id = "bigimg2" src="https://play-lh.googleusercontent.com/EuiZnkT8aEKjXDLX74DTp1VRIwWaeRa8Dvo-LOGAxy1FPQ8GzABTIRenksiM-A7Oz48g" alt="">
+    <i class="fa-solid fa-magnifying-glass" id="searchicon" ></i><input class="searchBox" type="text" placeholder="Search for Products...">
+        <p id="delivery">Delivery in 1 Day
+            5666441,Banglore
+      </p>
+      <div id="navbaricon">
+       <a href=""> <i class="fa-regular fa-user" id="usericon"></i></a>
+        <a href="cart.html"><i id="bagicon" class="fa-solid fa-bag-shopping"></i></a>
+        <p id = "bagcount"></p>
+      </div>
+</div>
+<div id="navbarbottom">
+    <select name="" id="select">
+        <option value="">Shop by Category</option>
+        <option value="">Fruits & Vegetables</option> 
+        <option value="">Foodgrains,Oil & Masala</option>
+        <option value="">Bakery, Cakes & Dairy</option>
+        <option value="">Beverages</option>
+        <option value="">Snacks & Brandes Foods</option>
+        <option value="">Beauty & Hygiene</option>
+        <option value="">Cleaning & Household</option>
+        <option value="">Kitchen, Garden & Pets</option>
+        <option value="">Eggs,Meat & Fish</option>
+        <option value="">Gourmet & World Food</option>
+        <option value="">Baby Care</option> 
+    </select>
+    <a href=""><p>Exotic Fruits & Vegg...</p></a>
+    <a href=""><p>Tea</p></a>
+    <a href=""><p>Ghee</p></a>
+    <a href=""><p>Nandhini</p></a>
+    <a href=""><p>Fresh Vegetables</p></a>
+    <img src="http://www.bigbasket.com/media/uploads/banner_images/hp_cmc_m_bbstar_28_220921_all.png" alt="">
+    <img src="http://www.bigbasket.com/media/uploads/banner_images/hp_cmc_m_offer_28_220921_all.png" alt="">
+</div>`
+}
+
+
+let footer = () => {
+    return ` <div id="firstBox">
+    <div>
+        <div id="firstLink">
+            <div>
+                <p>Bigbasket</p>
+                <a href=""><p>About Us</p></a>
+                <a href=""><p>In News</p></a>
+                <a href=""><p>Green bigbasket</p></a>
+                <a href=""><p>Privacy Policy</p></a>
+                <a href=""><p>Affiliate</p></a>
+                <a href=""><p>Terms and Conditions</p></a>
+                <a href=""><p>bb Daily</p></a>
+            </div>
+            <div>
+                <p>Help</p>
+                <a href=""><p>FAQs</p></a>
+                <a href=""><p>Contact Us</p></a>
+                <a href=""><p>bb Wallet FAQs</p></a>
+                <a href=""><p>bb Wallet T&Cs</p></a>
+                <a href=""><p>Vendor Connect</p></a>
+            </div>
+        </div>
+    </div>
+
+   <div id="socoalContainer">
+       <img class="bigbasketImg" src="https://sellersupport.in/wp-content/uploads/2021/05/bigbasket.png" alt="">
+       <div class="download">
+            <img src="https://www.freepnglogos.com/uploads/play-store-logo-png/play-store-logo-nisi-filters-australia-11.png" alt="">
+            <img class ="apple" src="https://cdn.freebiesupply.com/logos/large/2x/download-on-the-app-store-apple-logo-png-transparent.png" alt="">
+        </div>
+        <div id="social">
+            <img src="https://w7.pngwing.com/pngs/152/951/png-transparent-social-media-discovery-canyon-campus-school-computer-icons-facebook-desktop-small-fresh-background-thumbnail.png" alt="">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiz5_zr5bhtmyvwC2T0BO9N8R9fgk3I79SyLzDZR1hVYl6JhAc7qIYVYv1AEc_m_gWdwM&usqp=CAU" alt="">
+            <img src="https://cdn3.iconfinder.com/data/icons/picons-social/57/43-twitter-512.png" alt="">
+        </div>
+        <div id = "vender">
+            <p>Vendor Connect</p>
+            <button>Become a Seller</button>
+        </div>
+   </div>
+
+</div>
+
+
+    <hr id="hrdashed1">
+    <div id="tags">
+        <h4 class="cityhead">Cities We Serve</h4>
+        <p>Ujjain
+            |
+            Madurai
+            |
+            Shambhu
+            |
+            Sircilla District
+            |
+            Pratapgarh District
+            |
+            South 24 Parganas District
+            |
+            Muzaffarnagar District
+            |
+            Meerut District
+            |
+            Ludhiana District
+            |
+            Hooghly District
+            |
+            Gonda District
+            |
+            Fatehgarh Sahib District
+            |
+            Baghpat District
+            |
+            Amroha District
+            |
+            Amravati
+            |
+            Jamshedpur
+            |
+            Kolhapur
+            |
+            Kalaburagi
+            |
+            Guwahati
+            |
+            Bhubaneshwar-Cuttack
+            |
+            Bhiwadi
+            |
+            Sonipat
+            |
+            Bahadurgarh
+            |
+            Raichur
+            |
+            Sindudurg
+            |
+            Sangareddy
+            |
+            Khammam
+            |
+            Jadcherla
+            |
+            Tiruppur
+            |
+            Hubli
+            |
+            Puducherry
+            |
+            Tumakuru
+            |
+            Udham-Singh-Nagar
+            |
+            Nellore
+            |
+            Ambala
+            |
+            Agra
+            |
+            Rajkot
+            |
+            Sikar
+            |
+            Nadia
+            |
+            Bardoli
+            |
+            Sultanpur District
+            |
+            Thrissur
+            |
+            Raebareli District
+            |
+            Raipur
+            |
+            Sambhal District
+            |
+            Siddipet District
+            |
+            Faridabad
+            |
+            Prakasam
+            |
+            Nalgonda
+            |
+            Karnal
+            |
+            Panipat
+            |
+            Rohtak
+            |
+            Renigunta
+            |
+            Patiala
+            |
+            Trichy
+            |
+            Bulandshahr
+            |
+            Prakasam District
+            |
+            Surat
+            |
+            Mysore
+            |
+            Vadodara
+            |
+            Pune
+            |
+            Hyderabad
+            |
+            Kolkata
+            |
+            Chennai
+            |
+            Patan
+            |
+            Siddhpur
+            |
+            Mumbai
+            |
+            Nashik
+            |
+            Kalol
+            |
+            Kadi
+            |
+            Himatnagar
+            |
+            Mehsana
+            |
+            Raipur
+            |
+            Amaravathi
+            |
+            Allahabad
+            |
+            Jodhpur
+            |
+            Palakkad
+            |
+            Palwal
+            |
+            Kalinga Nagar
+            |
+            Mahabubnagar
+            |
+            Korba
+            |
+            Vijayawada-Guntur
+            |
+            Pilakhua District
+            |
+            Bhopal
+            |
+            Jaipur
+            |
+            Ahmedabad-Gandhinagar
+            |
+            Noida-Ghaziabad
+            |
+            Meerut
+            |
+            Vizianagaram
+            |
+            Medak District
+            |
+            East Godavari District
+            |
+            Nizamabad District
+            |
+            Kamareddy District
+            |
+            Lucknow Rural
+            |
+            Noida Rural
+            |
+            Chennai Rural
+            |
+            Vizag Rural
+            |
+            Nagpur
+            |
+            Kochi
+            |
+            Visnagar
+            |
+            Itarsi
+            |
+            Khordha
+            |
+            Dewas
+            |
+            kolar
+            |
+            Patna
+            |
+            Mandideep
+            |
+            Coimbatore
+            |
+            Kurnool
+            |
+            Suryapet District
+            |
+            Hosur
+            |
+            Eluru
+            |
+            Ghaziabad
+            |
+            Muzzafarpur
+            |
+            Delhi
+            |
+            Gurgaon
+            |
+            Bangalore
+            |
+            Chikkaballapura District
+            |
+            Srikakulam Disctrict
+            |
+            Vizianagaram Disctrict
+            |
+            Yanam District
+            |
+            Modinagar District
+            |
+            Puri
+            |
+            Gautam Buddha Nagar District
+            |
+            Anantapur District
+            |
+            Unjha
+            |
+            Dhone
+            |
+            Halflong
+            |
+            Parwanoo
+            |
+            Jajpur
+            |
+            Krishna District
+            |
+            Ashta
+            |
+            West-Godavari
+            |
+            Guntur-Rural
+            |
+            Kota
+            |
+            Barabanki District
+            |
+            Visakhapatnam District
+            |
+            Indore
+            |
+            Mhow
+            |
+            Visakhapatnam
+            |
+            Ludhiana
+            |
+            Hapur
+            |
+            Chandigarh Tricity
+            |
+            East-Godavari-2
+            |
+            Chittoor District
+            |
+            Tiruvallur District
+            |
+            Bidar District
+            |
+            Sangareddy District
+            |
+            Ayodhya District
+            |
+            Hoshangabad
+            |
+            Madanapalli
+            |
+            Tirupati
+            |
+            Miryalaguda
+            |
+            Kalwakurthy
+            |
+            Hajipur
+            |
+            Kadapa
+            |
+            Bhadrak
+            |
+            Gurugram Rural
+            |
+            Indore Rural
+            |
+            Guwahati Rural
+            |
+            Bhopal Rural
+            |
+            Hyderabad Rural
+            |
+            Coimbatore Rural
+            |
+            Bhubaneswar Rural
+            |
+            Bangalore Rural
+            |
+            Kolkata Rural
+            |
+            Kochi Rural
+            |
+            Kanpur
+            |
+            Lucknow
+            |
+            Anand
+            |
+            Saran
+            |
+            Nagpur Rural
+            |
+            Patna Rural
+            |
+            Mumbai Rural
+            |
+            Mysore Rural
+            |
+            Vadodara Rural
+            |
+            Jaipur Rural
+            |
+            Pune Rural
+            |
+            Surat Rural
+            |
+            Aligarh District
+            |
+            Amethi District
+            |
+            Jammu Kashmir
+            |
+            Katihar
+            |
+            Kottayam
+            |
+            Rajamahendravaram
+            |
+            Kakinada
+            |
+            Kundli
+            |
+            Sahibganj
+            |
+            Salcete
+            |
+            Allahabad
+            |
+            DehraDun
+            |
+            Chittor District
+            |
+            Saharanpur District
+            |
+            Aligarh</p>
+    </div>
+
+   <hr id="hrdashed">
+   <div id="bigSeccondLinkContainer">
+    <div id="seccondBox">
+        <div>
+            <p>Popular Categories</p>
+            <div id="seccondLink">
+                <div>
+                    <a href=""><p>Sunflower Oils</p></a>
+                    <a href=""><p>Milk</p></a>
+                    <a href=""><p>Organic F&V</p></a>
+                    <a href=""><p>Floor Cleaners</p></a>
+                    <a href=""><p>Frozen Veg Food</p></a>
+                </div>
+                <div>
+                    <a href=""><p>Wheat Atta</p></a>
+                    <a href=""><p>Health Drinks</p></a>
+                    <a href=""><p>Namkeen</p></a>
+                    <a href=""><p>Other Juices</p></a>
+                    <a href=""><p>Diapers & Wipes</p></a>
+                </div>
+                <div>
+                    <a href=""><p>Ghee</p></a>
+                    <a href=""><p>Flakes</p></a>
+                    <a href=""><p>Eggs</p></a>
+                    <a href=""><p>Leafy Vegetables</p></a>
+                </div>
+        </div>
+            </div>
+    
+            <div id="seccondBox">
+                <div>
+                    <p>Popular Brands</p>
+                    <div id="seccondLink">
+                        <div>
+                            <a href=""><p>Amul</p></a>
+                            <a href=""><p>Red Bull</p></a>
+                            <a href=""><p>Yummiez</p></a>
+                            <a href=""><p>Britannia</p></a>
+                            <a href=""><p>Haldirams</p></a>
+                        </div>
+                        <div>
+                            <a href=""><p>Nescafe</p></a>
+                            <a href=""><p>elite cake</p></a>
+                            <a href=""><p>Yera</p></a>
+                            <a href=""><p>Wow Momo</p></a>
+                            <a href=""><p>Ferrero</p></a>
+                        </div>
+                        <div>
+                            <a href=""><p>MTR</p></a>
+                            <a href=""><p>Pediasure</p></a>
+                            <a href=""><p>Yakult</p></a>
+                            <a href=""><p>Fortune</p></a>
+                            <a href=""><p>Lays</p></a>
+                        </div>
+                    </div>
+                </div>
+               </div>
+       </div>
+       <a class="showmore" href="">Show more+</a>`
+}
+export {navbar, navbar2, footer};
